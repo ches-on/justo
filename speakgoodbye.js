55351430e37723802dedc59ccb0e7874200cdb15
ches-on <113528339+ches-on@users.noreply.github.com>
2023-02-18 09:12:21 +0300

    TASK 2

---
+++ speakgoodbye.js
@@ -0,0 +1,17 @@
+(var names=[yaakov, John, Jane, Jason, Paul, Frank, Larry, Paula, Laura, Jim];
+console.log(names)
+for( i=0; i<names.length;i++)
+if( var firstletter=J){
+console.log "Good Bye" +names};
+else {
+console.log "Hello" +names}
+var speakword = "hello";
+var hellospeaker=(Window)
+var hellospeaker = function speak(name){
+    console.log(speakword + "" + names);
+    return(window);
+};)();
+var speakword = "Good Bye"
+var speakword= function speak(names){
+console.log(speakword + "" + names);
+};
